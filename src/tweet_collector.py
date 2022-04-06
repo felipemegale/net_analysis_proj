3a358de61dba67cb6513808ac62e2432eccceb25
Felipe Megale <felipe.megale17@gmail.com>
2022-04-06 07:44:28 -0400

    minor adjustments

--- src/tweet_collector.py
+++ src/tweet_collector.py
@@ -4,10 +4,13 @@ Python 3.9.7 (default, Sep 16 2021, 13:09:58)
 '''
 import os
 from time import sleep
+from datetime import datetime
 import concurrent.futures
 import json
-import pandas as pd
 from requests_oauthlib import OAuth1Session
+import csv
+
+begin = datetime.now()
 
 BASE_DATASETS_PATH='../FakeNews_Dataset/CSV'
 POLITIFACT_REAL_DATASET='sampled_real_news.csv'
@@ -47,6 +50,7 @@ def perform_request(tid):
 
 for real_news_row in politifact_real[1:]:
     splitted_row = real_news_row.split(',')
+    politifact_id = splitted_row[0]
     news_url = splitted_row[1]
     tweet_ids_to_collect = splitted_row[3].split('\t')
 
@@ -56,27 +60,28 @@ for real_news_row in politifact_real[1:]:
             future_to_tweet = {executor.submit(perform_request, tid): tid for tid in tweet_ids_to_collect}
             for future in concurrent.futures.as_completed(future_to_tweet):
                 tweet_data = future_to_tweet[future]
+                print('REAL_COLLECTED len', len(REAL_COLLECTED_DATA))
                 try:
                     data = future.result()
-                except Exception as e:
-                    print('%r generated an exception %s' % (tweet_data,e))
-                else:
                     if data.status_code == 200:
                         resp = json.loads(data.content)
-
                         try:
                             data = resp['data']
-                            REAL_COLLECTED_DATA.append(data['author_id'], data['text'], news_url, REAL_NEWS)
                         except:
                             pass
+                        else:
+                            REAL_COLLECTED_DATA.append([politifact_id, data['author_id'], data['text'], news_url, REAL_NEWS])
                     elif data.status_code == 429:
                         print(data.headers)
                         sleep(10)
                     else:
                         pass
+                except Exception as e:
+                    print('%r generated an exception %s' % (tweet_data,e))
 
 for fake_news_row in politifact_fake[1:]:
     splitted_row = fake_news_row.split(',')
+    politifact_id = splitted_row[0]
     news_url = splitted_row[1]
     tweet_ids_to_collect = splitted_row[3].split('\t')
 
@@ -86,27 +91,35 @@ for fake_news_row in politifact_fake[1:]:
             future_to_tweet = {executor.submit(perform_request, tid): tid for tid in tweet_ids_to_collect}
             for future in concurrent.futures.as_completed(future_to_tweet):
                 tweet_data = future_to_tweet[future]
+                print('FAKE_COLLECTED len', len(FAKE_COLLECTED_DATA))
                 try:
                     data = future.result()
-                except Exception as e:
-                    print('%r generated an exception %s' % (tweet_data,e))
-                else:
                     if data.status_code == 200:
                         resp = json.loads(data.content)
 
                         try:
                             data = resp['data']
-                            REAL_COLLECTED_DATA.append(data['author_id'], data['text'], news_url, REAL_NEWS)
                         except:
                             pass
+                        else:
+                            FAKE_COLLECTED_DATA.append([politifact_id, data['author_id'], data['text'], news_url, FAKE_NEWS])
                     elif data.status_code == 429:
                         print(data.headers)
                         sleep(10)
                     else:
                         pass
+                except Exception as e:
+                    print('%r generated an exception %s' % (tweet_data,e))
 
-real_df = pd.DataFrame(REAL_COLLECTED_DATA)
-fake_df = pd.DataFrame(FAKE_COLLECTED_DATA)
+with open(f'collected_real_news.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerows(REAL_COLLECTED_DATA)
 
-real_df.to_csv('real_collected.csv', index=False)
-fake_df.to_csv('fake_collected.csv', index=False)
+with open(f'collected_fake_news.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerows(FAKE_COLLECTED_DATA)
+
+end = datetime.now()
+
+print("Started:", begin.strftime("%H:%M:%S"))
+print("Ended:", end.strftime("%H:%M:%S"))
